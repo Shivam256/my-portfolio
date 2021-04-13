05c17c0f8cd720c9ad4f98e0435e8ddd84f7edab
Shivam256 <shivamgavandi256@gmail.com>
2021-04-13 10:45:34 +0530

    creted the about section and added responsiveness

--- script.js
+++ script.js
@@ -1,5 +1,21 @@
 // import curDot from 'cursor-dot';
 new WOW().init();
+const img = document.querySelector(".img-container");
+const btn = document.querySelector(".explore-btn");
+const greet = document.querySelector(".greeting");
+
+
+//gatting the time of day for greeting
+var today = new Date()
+var curHr = today.getHours()
+
+if (curHr < 12) {
+  greet.textContent = "GOOD MORNING";
+} else if (curHr < 18) {
+  greet.textContent = "GOOD AFTERNOON";
+} else {
+  greet.textContent = "GOOD EVENING";
+}
 
 
 //=====================mycursor ====================
@@ -17,6 +33,9 @@ new WOW().init();
 //     cursor.classList.remove("expand");
 //   },500);
 // })
+
+//============navbar-code===============================
+
 
 
 // =========================navbar code======================
@@ -37,12 +56,11 @@ function addRequiredClass() {
 window.onload = addRequiredClass
 
 if (window.innerWidth > 860) {
-  const btn = document.querySelector(".explore-btn");
-  const greet = document.querySelector(".greeting");
-  const img = document.querySelector(".img-container");
   const name1 = document.querySelector(".name");
   const def = document.querySelector(".def");
   const navElem = document.querySelectorAll("ul.nav-list li a");
+  const aboutContainer = document.querySelectorAll(".about-container");
+  const aboutHead = document.querySelector(".about-heading");
 
   const cursor = curDot({
     diameter: 80,
@@ -78,7 +96,7 @@ if (window.innerWidth > 860) {
   })
 
   cursor.over(img, {
-    scale:1.4,
+    scale: 1.4,
     // background:"#F72585"
     background: "#08da7a"
   })
@@ -90,7 +108,43 @@ if (window.innerWidth > 860) {
     })
   }
 
+  for(let i of aboutContainer){
+    cursor.over(i,{
+      scale:1.5,
+      background:"#8DF648"
+    })
+  }
+
+  cursor.over(aboutHead,{
+    scale:0.8,
+    background:"#3e045b"
+  })
+
+  // window.onscroll = function () {
+  //   myFunction()
+  // };
+  // var navbar = document.getElementById("navbar");
+  // var sticky = navbar.offsetTop;
+
+  // function myFunction() {
+  //   if (window.pageYOffset >= sticky) {
+  //     navbar.classList.add("sticky")
+  //   } else {
+  //     navbar.classList.remove("sticky");
+  //   }
+  // }
+
 }
+
+if(window.innerWidth < 860){
+  const pD = document.querySelectorAll(".about-info p");
+  for(let i of pD){
+    i.textContent = "";
+  }
+}
+
+
+
 
 let hamburger = document.querySelector('.hamburger')
 let mobileNav = document.querySelector('.nav-list')
@@ -106,10 +160,15 @@ hamburger.addEventListener('click', function () {
     bars[1].style.opacity = '0'
     bars[2].style.transform = 'rotate(-45deg)'
     isActive = true
+    img.style.opacity = "0"
+    btn.style.opacity = "0"
+
   } else {
     bars[0].style.transform = 'rotate(0deg)'
     bars[1].style.opacity = '1'
     bars[2].style.transform = 'rotate(0deg)'
+    img.style.opacity = "1"
+    btn.style.opacity = "1"
     isActive = false
   }
 
@@ -117,3 +176,6 @@ hamburger.addEventListener('click', function () {
 })
 
 //================================================================
+window.onbeforeunload = function () {
+  window.scrollTo(0, 0);
+}
